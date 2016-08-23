8ab9db1c38375ee9abf01345c704371f2c7e9f7e
Karla Aguilar <kn.aguilar7@gmail.com>
2016-08-23 07:41:18 -0400

    feat: add duo cat clicker

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 var clickOne = 0;
+var clickTwo = 0;
 
 $('#my-cat').click(function() {
 	clickOne++;
   	$("#click-one").html(clickOne);
 });
 
+$('#your-cat').click(function() {
+    clickTwo++;
+    $("#click-two").html(clickTwo);
+});
+
+var myCatName = "Keeta";
+var yourCatName = "Cato";
+
 $('#cat-one').append(myCatName);
+$('#cat-two').append(yourCatName);
