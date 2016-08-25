b142f0bf7e997061dc805ee11cef814ad5022a0e
Karla Aguilar <kn.aguilar7@gmail.com>
2016-08-24 22:22:52 -0400

    feat: reqirement changes 2 - add list of cat names

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
-var clickOne = 0;
-var clickTwo = 0;
+// var clickOne = 0;
+// var clickTwo = 0;
 
-$('#my-cat').click(function() {
-	clickOne++;
-  	$("#click-one").html(clickOne);
-});
+var catNames = ['Keeta', 'Cato', 'Salem', 'Twix', 'Gordi'];
+var name;
 
-$('#your-cat').click(function() {
-    clickTwo++;
-    $("#click-two").html(clickTwo);
-});
+//give each an idea to later be able to control their clicks
+for (var i = 0; i < catNames.length; i++) {
+	name = catNames[i];
+	$('ul').append('<li id="'+ name + '">' + name + '</li>');
+}
 
-var myCatName = "Keeta";
-var yourCatName = "Cato";
+	// $('ul').append('<img src="images/keeta.jpg" width="350" height="250">');
 
-$('#cat-one').append(myCatName);
-$('#cat-two').append(yourCatName);
+
+
+
+
+
+// $('#my-cat').click(function() {
+// 	clickOne++;
+//   	$("#click-one").html(clickOne);
+// });
+
+// $('#your-cat').click(function() {
+//     clickTwo++;
+//     $("#click-two").html(clickTwo);
+// });
+
+// var myCatName = "Keeta";
+// var yourCatName = "Cato";
+
+// $('#cat-one').append(myCatName);
+// $('#cat-two').append(yourCatName);
